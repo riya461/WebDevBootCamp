1703f02ec957448fe883ff76be40c5c901fb2230
Riya Sabu <riyasabures@gmail.com>
2023-05-17 03:10:33 +0530

    adding NodeJS chapter

---
+++ NodeJS/index.js
@@ -0,0 +1,10 @@
+// const fs = require("fs");
+
+// fs.copyFileSync("file1.txt","file2.txt")
+
+// using a module in js
+
+var superheroes = require("superheroes");
+
+var myhero = superheroes.random()
+console.log(myhero)
