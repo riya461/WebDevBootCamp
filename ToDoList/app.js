9db03da8b6aefb083ec84dbdaf14df96b5ca1134
Riya Sabu <riyasabures@gmail.com>
2023-05-20 09:33:41 +0530

    adding the database

--- ToDoList/app.js
+++ ToDoList/app.js
@@ -1,10 +1,40 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-
+const mongoose = require('mongoose')
 const app = express();
-var items =[];
+
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended:true}))
-app.set('view engine', 'ejs');
+app.use(express.static("public"))
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true})
+
+// new items schema with one field name of type string 
+
+const itemsSchema = {
+    name: String
+}
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to To Do List!"
+})
+const item2 = new Item({
+    name: "Hit + to delete"
+})
+
+const item3 = new Item({
+    name: "You can check to cross off"
+})
+const defaultItems = [item1, item2, item3]
+Item.insertMany(defaultItems), function(err){
+    if(err){
+        console.log(err)
+    }
+    else{
+        console.log("Succesfully saved default items to DB")
+    }
+}
 app.get('/', (req,res)=>{
     var today = new Date();
      
